8d936cc907eb73bfd798dc1f8f7899097af57896
Mike Copple <michaelcopple@gmail.com>
2018-02-18 08:23:10 -0500

    scene loading added

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,13 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] float delayTime = 1.5f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State {Alive, Dying, Transcending};
+    State state = State.Alive;
 
 	// Use this for initialization
 	void Start () {
@@ -18,27 +24,43 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Thrust();
-        Rotate();
-
-	}
+        //todo stop sound on death
+        if (state != State.Dying) {
+            Thrust();
+            Rotate();
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        switch (collision.gameObject.tag)
-        {
+        if (state != State.Alive) {
+            return;
+        }
+
+        switch (collision.gameObject.tag) {
             case "Friendly":
                 //do nothing
-                print("Friendly");
                 break;
-            case "Fuel":
-                print("Fuel");
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextLevel", delayTime);
                 break;
             default:
-                print("Dead");
                 //kill player
+                state = State.Dying;
+                Invoke("LoadFirstLevel", delayTime);
                 break;
         }
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1); // todo allow for more than 2 levels
     }
 
     private void Thrust()
